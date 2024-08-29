ddbddb56b9124198b744c73d0bb60b145fbabf72
vibhurj <91533492+vibhurj@users.noreply.github.com>
2024-08-29 11:27:28 +0530

    Add files via upload

---
+++ dashboard2.js
@@ -0,0 +1,46 @@
+function sdbarshw(){
+    var shcl=document.getElementById('iconshcl')
+    if (shcl.classList.contains("bi-list"))
+{
+    shcl.classList.replace("bi-list","bi-x")
+    document.getElementById("sdbar").style.marginLeft="0px"
+    document.getElementById("log").style.marginLeft="0px"
+}
+else{
+    shcl.classList.replace("bi-x","bi-list")
+    document.getElementById("sdbar").style.marginLeft="-260px"
+
+    document.getElementById("log").style.marginLeft="-220px"
+}
+    
+}
+function tblclick(){
+    v=document.getElementById("tbl1").style.display
+    if(v=='none'){
+        document.getElementById("tbl1").style.display="block"
+    }
+    else
+    {
+        document.getElementById("tbl1").style.display="none"
+    }
+}
+function tblclick2(){
+    v=document.getElementById("tbl2").style.display
+    if(v=='none'){
+        document.getElementById("tbl2").style.display="block"
+    }
+    else
+    {
+        document.getElementById("tbl2").style.display="none"
+    }
+}
+function tblclick3(){
+    v=document.getElementById("tbl3").style.display
+    if(v=='none'){
+        document.getElementById("tbl3").style.display="block"
+    }
+    else
+    {
+        document.getElementById("tbl3").style.display="none"
+    }
+}
